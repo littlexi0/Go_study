6c2ca81e28da97abc4e76cdf8381823be118ffa4
Little Xi <1463993001@qq.com>
2023-07-14 23:37:53 +0800

    分离模块

---
+++ Gin/Gin_backed/common/database.go
@@ -0,0 +1,39 @@
+package common
+
+import (
+	"fmt"
+	"mymodule/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() *gorm.DB {
+	// driverName := "mysql"
+	host := "localhost"
+	port := "3306"
+	database := "ginessential"
+	username := "root"
+	password := "123456"
+	charset := "utf8"
+	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset)
+	db, err := gorm.Open(mysql.Open(args), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database,err: " + err.Error())
+	}
+	db.AutoMigrate(&model.User{})
+	DB = db
+	return db
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
--- Gin/Gin_backed/main.go
+++ Gin/Gin_backed/main.go
@@ -1,112 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"net/http"
-	"time"
+	"mymodule/common"
+	"mymodule/route"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
-type User struct {
-	gorm.Model
-	Name      string `gorm:"type:varchar(20);not null"`
-	Telephone string `gorm:"type:varchar(11);not null;unique"`
-	Password  string `gorm:"size:255;not null"`
-}
-
 func main() {
-	db := InitDB()
+	common.InitDB()
 	r := gin.Default()
-	r.POST("/api/auth/reginster", func(ctx *gin.Context) {
-		name := ctx.PostForm("name")
-		telephone := ctx.PostForm("telephone")
-		password := ctx.PostForm("password")
+	r = route.CollectRoute(r)
+	panic(r.Run())
 
-		//手机号验证
-		if len(telephone) != 11 {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-				"code": 422,
-				"msg":  "手机号必须为11位",
-			})
-			return
-		}
-		//密码验证
-		if len(password) < 6 {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-				"code": 422,
-				"msg":  "密码不能少于6位",
-			})
-			return
-		}
-		//如果名称没有传,给一个10位的随机字符串
-		if len(name) == 0 {
-			name = RandomString(10)
-		}
-
-		//判断手机号是否存在
-		if isTelephoneExist(db, telephone) {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-				"code": 422,
-				"msg":  "用户已经存在",
-			})
-			return
-		}
-		// 创建用户
-		newUser := User{
-			Name:      name,
-			Telephone: telephone,
-			Password:  password,
-		}
-		db.Create(&newUser)
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "好哒",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-}
-
-func isTelephoneExist(db *gorm.DB, telephone string) bool {
-	var user User
-	db.Where("telephone = ?", telephone).First(&user)
-	if user.ID != 0 {
-		return true
-	}
-	return false
-}
-
-func RandomString(n int) string {
-	var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	result := make([]byte, n)
-	rand.Seed(time.Now().Unix())
-	for i := range result {
-		result[i] = letters[rand.Intn(len(letters))]
-	}
-	return string(result)
-}
-
-func InitDB() *gorm.DB {
-	// driverName := "mysql"
-	host := "localhost"
-	port := "3306"
-	database := "ginessential"
-	username := "root"
-	password := "123456"
-	charset := "utf8"
-	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
-		username,
-		password,
-		host,
-		port,
-		database,
-		charset)
-	db, err := gorm.Open(mysql.Open(args), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database,err: " + err.Error())
-	}
-	db.AutoMigrate(&User{})
-	return db
 }
