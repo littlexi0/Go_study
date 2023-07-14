0f2cf74322199d6b3a075ccf778c868070e12d65
Little Xi <1463993001@qq.com>
2023-07-14 17:02:23 +0800

    连接数据库upd

--- Gin/vue_fonted/vue-project/src/App.vue
+++ Gin/vue_fonted/vue-project/src/App.vue
@@ -10,12 +10,48 @@ import TheWelcome from './components/TheWelcome.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
+    <button @click="btn">按钮</button>
   </header>
 
   <main>
     <TheWelcome />
   </main>
 </template>
+
+<script>
+  
+  import { defineComponent } from 'vue'
+  // import {axios} from './utils/request'
+  // import { useRouter } from 'vue-router'
+  import axios from 'axios';
+  export default defineComponent({
+    name: 'App',
+    setup() {
+      const router = useRouter()
+      const btn = () => {
+        router.push('/about')
+      }
+      return {
+        btn
+      }
+    },
+    methods: {
+      btn() {
+        // axios.post('http://localhost:8080/form_post/',{
+        //   message: 'hello',
+        //   nick: 'nickname'
+        // }).then(res => {
+        //   console.log(res)
+        // })
+        axios.get('http://localhost:8080/welcome?firstname=little&lastname=xi').then(res => {
+          console.log(res)
+        })
+      }
+    }
+  })
+
+
+</script>
 
 <style scoped>
 header {
